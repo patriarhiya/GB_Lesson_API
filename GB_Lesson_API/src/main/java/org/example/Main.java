7ac8f0ab600a56fe74dec4a7d6003ad24c70ee0d
Anatoly Sber <agenfedotov@sberbank.ru>
2024-05-15 16:46:37 +0300

    Update Main.java

--- GB_Lesson_API/src/main/java/org/example/Main.java
+++ GB_Lesson_API/src/main/java/org/example/Main.java
@@ -161,7 +161,7 @@ class BubbleSort {
                 public String format(LogRecord record) {
                     LocalDateTime now = LocalDateTime.now();
                     Object DateTimeFormatter = now.format();
-                    String format = now.format(DateTimeFormatter.ISO_DATE_TIME2);
+                    String format = now.format(DateTimeFormatter.ISO_DATE_TIME3);
                     String s = String.valueOf(LocalDate.now() + " " + hourNow + "\n");
                     return s;
                 }
